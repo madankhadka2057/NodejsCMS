a274eb9c122ff78eb0f4cf32ce60f83535eb983e
madankhadka2057 <101637233+madankhadka2057@users.noreply.github.com>
2023-09-24 22:50:55 +0545

    fix error

--- controller/auth/userController.js
+++ controller/auth/userController.js
@@ -1,5 +1,7 @@
 const { users } = require("../../model");
 const bcrypt = require("bcryptjs");
+
+
 exports.renderRegister = (req, res) => {
   res.render("register");
 };
@@ -19,6 +21,7 @@ exports.register = async (req, res) => {
       return res.send("User already exist");
     }
   }
+
   if (confirm_password != password) {
     console.log("Passwird Doesn't match please try again");
     return res.send("password doesn't match");
@@ -40,7 +43,6 @@ exports.renderLogin = (req, res) => {
 };
 
 
-
 //~checking loging
 exports.login = async (req, res) => {
   const { email, password } = req.body;
@@ -55,8 +57,9 @@ exports.login = async (req, res) => {
     if (associatedDataWithEmail.length == 0) {
       res.send("User doesn't associated with this email");
     } else {
+      const assocaitedPasswordWithEmail = associatedDataWithEmail[0].password;
       const isMatch = bcrypt.compareSync(password, assocaitedPasswordWithEmail);
-      console.log("isMatch:", isMatch);
+
       if (isMatch) {
         res.send("Successfully login");
       } else {

f6419be4287206bf481990019bf8eb40b69a1ba1
madankhadka2057 <101637233+madankhadka2057@users.noreply.github.com>
2023-09-24 22:41:50 +0545

    add login

--- controller/auth/userController.js
+++ controller/auth/userController.js
@@ -5,6 +5,19 @@ exports.renderRegister = (req, res) => {
 };
 exports.register = async (req, res) => {
   const { name, email, password, confirm_password } = req.body;
+  const checkEmail = await users.findAll({
+    where: {
+      email: email,
+    },
+  });
+  if (checkEmail.length != 0) {
+    const existingEmail = checkEmail[0].email;
+    console.log("mail is:",existingEmail);
+    if (existingEmail === email) {
+      return res.send("User already exist");
+    }
+  }
+
   if (confirm_password != password) {
     console.log("Passwird Doesn't match please try again");
     return res.send("password doesn't match");
@@ -18,16 +31,33 @@ exports.register = async (req, res) => {
     res.redirect("/login");
   }
 };
-
-exports.renderLogin=(req,res)=>{
-    res.render('login')
-}
-exports.login=async(req,res)=>{
-    console.log(req.body);
-    const {email,password}=req.body;
-    await users.create({
-        email,
-        password
-    })
-    res.redirect('/')
-}
+//! render Login from
+exports.renderLogin = (req, res) => {
+  res.render("login");
+};
+//~checking loging
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return console.log(" Please insert email or password");
+  } else {
+    const associatedDataWithEmail = await users.findAll({
+      where: {
+        email: email,
+      },
+    });
+    if (associatedDataWithEmail.length == 0) {
+      res.send("User doesn't associated with this email");
+    } else {
+      const isMatch = bcrypt.compareSync(password, assocaitedPasswordWithEmail);
+      console.log("isMatch:", isMatch);
+      if (isMatch) {
+        res.send("Successfully login");
+      } else {
+        // console.log("Password Doesn't match")
+        res.send("Password Doesn't match");
+      }
+    }
+  }
+  // res.redirect('/')
+};
--- model/index.js
+++ model/index.js
@@ -1,5 +1,5 @@
 const dbConfig = require("../config/dbConfig");
-const { Sequelize, DataTypes } = require("sequelize");
+const { Sequelize, DataTypes, HasMany, BelongsTo } = require("sequelize");
 
 // la sequelize yo config haru lag ani database connect gardey vaneko hae
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
@@ -31,6 +31,10 @@ db.sequelize = sequelize;
 // importing model files
 db.blogs = require("./blogModel.js")(sequelize, DataTypes);
 db.users = require("./userModel.js")(sequelize, DataTypes);
+
+//!making relation between them
+db.users.hasMany(db.blogs)
+db.blogs.belongsTo(db.users)
 
 db.sequelize.sync({ force: false }).then(() => {
   console.log("yes re-sync done");
